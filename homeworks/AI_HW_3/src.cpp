6d55f427110b07beac0cba895840ffd98194bd94
Alexander Makedonski <alemak2002@gmail.com>
2024-11-12 14:39:47 +0200

    Finished homework 3.

--- homeworks/AI_HW_3/src.cpp
+++ homeworks/AI_HW_3/src.cpp
@@ -6,24 +6,52 @@
 #include <algorithm>
 #include <float.h>
 #include <unordered_set>
+#include <chrono>
+#include <sstream>
+#include <fstream>
+#include <charconv>
+#include <optional>
 
 using namespace std;
 
 typedef vector<uint64_t> Path;
 
+/* Solution can be optimised a lot, since there is a lot of unneeded copying, but currently does the job pretty well for N <= 100, so I won't do it.*/
+
+/* I should have f**king done this in python. */
 
 #define TEST_PRINT 0
-#define TEST_CITIES 1
 
-/* Will use all given crossovers. Will generate more children in more crossovers are enabled. If none are, parents will just be copied.*/
-#define USE_OP_CROSSOVER 1
-#define USE_TP_CROSSOVER 1
-#define USE_PMX_CROSSOVER 1
-/* if 1 will use Roulette, if 0 - Rank-based*/
-#define USE_ROULETTE_SELECTION 0
+/* Will cross random parents if enabled, else will cross best parents.*/
+#define ENABLE_RANDOM_CROSSOVER 0
 
-random_device dev;
+/* Will use all given crossovers. Will generate more children in more crossovers are enabled.
+   If none are, parents will just be copied. */
+#define ENABLE_OP_CROSSOVER 1
+#define ENABLE_TP_CROSSOVER 1
+#define ENABLE_PMX_CROSSOVER 1
+/* if 1 will use Tournament, if 0 - Rank-based*/
+#define USE_TOURNAMENT_SELECTION 0
+
+#define MUTATION_CHANCE ((double) 10/100)
+/* swap mutation is always enabled */
+#define ENABLE_INSERTION_MUTATION 1
+#define ENABLE_REVERSE_MUTATION 1
+
+#define GENERATIONS 10
+#define ENABLE_TRUE_RANDOM 1
+
+/* Do not edit these */
+#define INITIAL_PATHS_SCALAR 600
+#define SELECT_WINNERS_DELIM (ENABLE_OP_CROSSOVER + ENABLE_TP_CROSSOVER + ENABLE_PMX_CROSSOVER + 1)
+#define NEW_RANDOM_GENERATIONS_SCALAR 0
+
+#if ENABLE_TRUE_RANDOM
+mt19937 rng(chrono::system_clock::now().time_since_epoch().count());
+#else
+random_device dev(12);
 mt19937 rng(dev());
+#endif // ENABLE_TRUE_RANDOM
 
 double get_random_real(const double min, const double max)
 {
@@ -63,6 +91,7 @@ class TravelingSalesmanSolver
 
 
 public:
+    TravelingSalesmanSolver(){};
     TravelingSalesmanSolver(vector<string> point_names, vector<pair<double, double>> points);
     TravelingSalesmanSolver(uint64_t n);
 
@@ -79,7 +108,7 @@ public:
     Path generate_random_path() const;
     vector<Path> generate_random_paths(uint64_t size) const;
     vector<Path> rank_based_selection(const vector<Path>& paths, uint64_t select_size) const;
-    vector<Path> roulette_selection(const vector<Path>& paths, uint64_t select_size) const;
+    vector<Path> tournament_selection(const vector<Path>& paths, uint64_t select_size) const;
     vector<Path> generate_children(const vector<Path>& parents) const;
     /* Partially mapped crossover */
     pair<Path, Path> PMX_crossover(const Path& parent1, const Path& parent2) const;
@@ -98,9 +127,9 @@ TravelingSalesmanSolver::TravelingSalesmanSolver(vector<string> point_names, vec
 
 TravelingSalesmanSolver::TravelingSalesmanSolver(uint64_t n)
 {
-    if (!((n > 0) && (n <= 100)))
+    if (!(n > 1))
     {
-        throw "Bad n error.";
+        throw std::bad_alloc();
     }
 
     this->points.resize(n);
@@ -108,7 +137,7 @@ TravelingSalesmanSolver::TravelingSalesmanSolver(uint64_t n)
 
     for (uint64_t i = 0; i < n; i++)
     {
-        this->points[i] = {get_random_real(0, 500),  get_random_real(0, 500)};
+        this->points[i] = {get_random_real(0, 5000),  get_random_real(0, 5000)};
         string point_mame = "(" + to_string(this->points[i].first) + ", " + to_string(this->points[i].second) + ")";
         this->point_names[i] = move(point_mame);
     }
@@ -170,9 +199,9 @@ void TravelingSalesmanSolver::print_path_names(const Path& path) const
 {
     for (uint64_t i = 0; i < path.size() - 1; i++)
     {
-        cout << this->point_names[i] << " -> ";
+        cout << this->point_names[path[i]] << " -> ";
     }
-    cout << this->point_names[path.size() - 1];
+    cout << this->point_names[path[path.size() - 1]];
     cout << endl;
 }
 
@@ -225,79 +254,81 @@ vector<Path> TravelingSalesmanSolver::rank_based_selection(const vector<Path>& p
 }
 
 
-vector<Path> TravelingSalesmanSolver::roulette_selection(const vector<Path>& paths, uint64_t select_size) const
+vector<Path> TravelingSalesmanSolver::tournament_selection(const vector<Path>& paths, uint64_t select_size) const
 {
     vector<Path> selected_paths;
-    vector<double> cumulative_fitness(paths.size() + 1, 0.0);
-    unordered_set<uint64_t> selected_indices;
+    vector<Path> copy_paths = paths;
+    // assert(select_size > 0);
+    uint64_t index_delim = paths.size()/select_size;
 
-    double min_path_length = this->path_length(paths[this->get_best_path_index(paths)]);
-    double max_path_length = this->path_length(paths[this->get_worst_path_index(paths)]);
+    const Path* current_path = &paths[0]; // kinda ugly, but will do
+    uint64_t best_length = this->path_length(paths[0]);
 
-    double total_fitness = 0;
-    for (uint64_t i = 0; i < paths.size(); i++)
+    for (uint64_t i = 1; i < paths.size(); i++)
     {
-        double relative_fitness =(max_path_length - this->path_length(paths[i])) / (max_path_length - min_path_length + 1e-9); // Add small value to avoid division by zero
-        total_fitness += relative_fitness;
-        cumulative_fitness[i + 1] = total_fitness;
-    }
-
-    for (uint64_t i = 0; i < select_size; i++)
-    {
-        double rand_val = get_random_real(0, total_fitness);
-        auto it = lower_bound(cumulative_fitness.begin(), cumulative_fitness.end(), rand_val);
-        uint64_t index = it - cumulative_fitness.begin();
-
-        if (index != 0) /* might get actual 0 */
+        if (i % index_delim == 0)
         {
-            index --;
+            selected_paths.push_back(*current_path);
+            current_path = &paths[i];
+            best_length = this->path_length(paths[i]);
         }
-
-        if (selected_indices.find(index) == selected_indices.end())
+        else if (this->path_length(paths[i]) < best_length)
         {
-            selected_paths.push_back(paths[index]);
-            selected_indices.insert(index);
+            best_length = this->path_length(paths[i]);
+            current_path = &paths[i];
         }
     }
-
+    selected_paths.push_back(*current_path);
     return selected_paths;
 }
 
 vector<Path> TravelingSalesmanSolver::generate_children(const vector<Path>& parents) const
 {
     vector<Path> result;
-    vector<uint64_t> indices_for_crossover(parents.size());
+    vector<uint64_t> path_lengths(parents.size());
+#if ENABLE_RANDOM_CROSSOVER
     iota(indices_for_crossover.begin(), indices_for_crossover.end(), 0);
     shuffle(indices_for_crossover.begin(), indices_for_crossover.end(), rng);
-
+#else
+    for (uint64_t i = 0; i < parents.size(); i++)
+    {
+        path_lengths[i] = this->path_length(parents[i]);
+    }
+    vector<uint64_t> indices_for_crossover = get_sorted_indices(path_lengths);
+#endif // ENABLE_RANDOM_CROSSOVER
 
     for (uint64_t i = 0; i < indices_for_crossover.size() - 1; i += 2)
     {
         const Path& parent1 = parents[indices_for_crossover[i]];
         const Path& parent2 = parents[indices_for_crossover[i + 1]];
 
-#if USE_OP_CROSSOVER
+#if ENABLE_OP_CROSSOVER
         pair<Path, Path> op_offspring = this->OP_crossover(parent1, parent2);
         result.push_back(op_offspring.first);
         result.push_back(op_offspring.second);
-#endif // USE_OP_CROSSOVER
+#endif // ENABLE_OP_CROSSOVER
 
-#if USE_TP_CROSSOVER
+#if ENABLE_TP_CROSSOVER
         pair<Path, Path> tp_offspring = this->TP_crossover(parent1, parent2);
         result.push_back(tp_offspring.first);
         result.push_back(tp_offspring.second);
 #endif // USE_TP_CROSSOVER
 
-#if USE_PMX_CROSSOVER
+#if ENABLE_PMX_CROSSOVER
         pair<Path, Path> pmx_offspring = this->PMX_crossover(parent1, parent2);
         result.push_back(pmx_offspring.first);
         result.push_back(pmx_offspring.second);
 #endif // USE_PMX_CROSSOVER
 
-#if !(USE_PMX_CROSSOVER || USE_TP_CROSSOVER || USE_OP_CROSSOVER)
+#if !(ENABLE_PMX_CROSSOVER || ENABLE_TP_CROSSOVER || ENABLE_OP_CROSSOVER)
         result.push_back(parent1);
         result.push_back(parent2);
-#endif // not (USE_PMX_CROSSOVER || USE_TP_CROSSOVER || USE_OP_CROSSOVER)
+#endif // not (USE_PMX_CROSSOVER || USE_TP_CROSSOVER || ENABLE_OP_CROSSOVER)
+    }
+
+    if (parents.size() % 2 == 1)  //keep copy of best parent
+    {
+        result.push_back(parents[indices_for_crossover[0]]);
     }
 
     return result;
@@ -481,15 +512,23 @@ pair<Path, Path> TravelingSalesmanSolver::OP_crossover(const Path& parent1, cons
 
 void TravelingSalesmanSolver::mutate(Path& path) const
 {
-    int mutation_type = get_random_int(1, 3);
+    double mutation_chance = get_random_real(0, 100);
+    if (mutation_chance > (MUTATION_CHANCE*100))
+    {
+        return;
+    }
+
+    uint64_t current_max_mutation = 1 + ENABLE_INSERTION_MUTATION + ENABLE_REVERSE_MUTATION;
+    uint64_t mutation_type = get_random_int(1, current_max_mutation);
     switch (mutation_type)
     {
-        case 1: /* Swap Mutation */
+        case 1:
         {
             swap(path[get_random_int(0, path.size() - 1)], path[get_random_int(0, path.size() - 1)]);
             break;
         }
-        case 2: /* Insertion Mutation */
+#if ENABLE_INSERTION_MUTATION
+        case 2:
         {
             uint64_t erase_at = get_random_int(0, path.size() - 1);
             uint64_t insert_at = get_random_int(0, path.size() - 2);
@@ -498,13 +537,21 @@ void TravelingSalesmanSolver::mutate(Path& path) const
             path.insert(path.begin() + insert_at, res);
             break;
         }
-        case 3: /* Inversion Mutation */
+#endif // ENABLE_INSERTION_MUTATION
+#if ENABLE_REVERSE_MUTATION
+#if ENABLE_INSERTION_MUTATION
+        case 3:
+#else
+        case 2:
+#endif // ENABLE_INSERTION_MUTATION
         {
             uint64_t start = get_random_int(0, path.size() - 2);
             uint64_t end = get_random_int(start + 1, path.size() - 1);
             reverse(path.begin() + start, path.begin() + end + 1);
             break;
         }
+
+#endif // ENABLE_REVERSE_MUTATION
     }
 }
 
@@ -515,11 +562,6 @@ void TravelingSalesmanSolver::mutate_paths(vector<Path>& paths) const
         this->mutate(path);
     }
 }
-
-#define INITIAL_PATHS_SCALAR 200
-#define SELECT_WINNERS_DELIM 5
-#define NEW_RANDOM_GENERATIONS_SCALAR 200
-#define GENERATIONS 10
 
 Path TravelingSalesmanSolver::find_solution() const
 {
@@ -535,24 +577,22 @@ Path TravelingSalesmanSolver::find_solution() const
         cout << "Number of generated paths: " << current_paths.size() << endl;
 #endif // TEST_PRINT
         uint64_t select_winners_size = current_paths.size()/SELECT_WINNERS_DELIM;
-#if USE_ROULETTE_SELECTION
-        current_paths = this->roulette_selection(current_paths, select_winners_size);
+#if USE_TOURNAMENT_SELECTION
+        current_paths = this->tournament_selection(current_paths, select_winners_size);
 #else
         current_paths = this->rank_based_selection(current_paths, select_winners_size);
 #endif // ROULETTE_SELECTION
-        vector<Path> mutated_children = this->generate_children(current_paths);
-        //vector<Path> children = this->generate_children(current_paths);
+        vector<Path> children = this->generate_children(current_paths);
         vector<Path> new_generation = this->generate_random_paths(n*NEW_RANDOM_GENERATIONS_SCALAR);
 
-        mutate_paths(mutated_children);
+        mutate_paths(children);
 
 #if TEST_PRINT
         cout << "Number of winners: " << current_paths.size() << endl;
         cout << "Number of children: " << children.size() << endl;
         cout << "Number of new_generation: " << new_generation.size() << endl;
 #endif // TEST_PRINT
-        current_paths.insert(current_paths.end(), mutated_children.begin(), mutated_children.end());
-        //current_paths.insert(current_paths.end(), children.begin(), children.end());
+        current_paths.insert(current_paths.end(), children.begin(), children.end());
         current_paths.insert(current_paths.end(), new_generation.begin(), new_generation.end());
 
         current_best_index = this->get_best_path_index(current_paths);
@@ -566,45 +606,108 @@ Path TravelingSalesmanSolver::find_solution() const
     return current_paths[current_best_index];
 }
 
+optional<double> convert_to_number(const string& str) // python is easier :(
+{
+    try
+    {
+        size_t idx;
+        double value = stod(str, &idx);
+
+        if (idx == str.size())
+        {
+            return value;
+        }
+    } catch (const invalid_argument& e)
+    {
+
+    } catch (const out_of_range& e)
+    {
+
+    }
+    return nullopt;
+
+}
+
+vector<string> read_CSV(const string& filename)
+{
+    vector<string> data;
+    ifstream file(filename);
+
+    string line;
+    while (getline(file, line)) {
+        vector<string> row;
+        stringstream lineStream(line);
+        string cell;
+
+        while (getline(lineStream, cell, ','))
+        {
+            data.push_back(cell);
+        }
+    }
+
+    return data;
+}
+
+vector<pair<double, double>> read_CSV_points(const string& filename)
+{
+    vector<pair<double, double>> points;
+    ifstream file(filename);
+    string line;
+
+    while (getline(file, line))
+    {
+        stringstream lineStream(line);
+        string cell;
+
+        while (getline(lineStream, cell, ' '))
+        {
+            stringstream cellStream(cell);
+            string xStr, yStr;
+            if (getline(cellStream, xStr, ',') && getline(cellStream, yStr))
+            {
+                optional<double> xOpt = convert_to_number(xStr);
+                optional<double> yOpt = convert_to_number(yStr);
+
+                if (xOpt && yOpt)
+                {
+                    points.push_back({*xOpt, *yOpt});
+                }
+            }
+        }
+    }
+
+    return points;
+}
+
 int main()
 {
-#if TEST_CITIES
-    vector<string> city_names =
-    {
-        "Aberystwyth",
-        "Brighton",
-        "Edinburgh",
-        "Exeter",
-        "Glasgow",
-        "Inverness",
-        "Liverpool",
-        "London",
-        "Newcastle",
-        "Nottingham",
-        "Oxford",
-        "Stratford"
-    };
+    string input;
+    cin >> input;
 
-    vector<pair<double, double>> points =
-    {
-        {0.190032E-03,-0.285946E-03},
-        {383.458,-0.608756E-03},
-        {-27.0206,-282.758},
-        {335.751,-269.577},
-        {69.4331,-246.780},
-        {168.521,31.4012},
-        {320.350,-160.900},
-        {179.933,-318.031},
-        {492.671,-131.563},
-        {112.198,-110.561},
-        {306.320,-108.090},
-        {217.343,-447.089},
-    };
+    TravelingSalesmanSolver solver;
 
-    TravelingSalesmanSolver solver(city_names, points);
-#else
-    TravelingSalesmanSolver solver(100);
-#endif // TEST_CITIES
+    optional<double> number = convert_to_number(input);
+    if (number)
+    {
+        uint64_t N = *number;
+        solver = TravelingSalesmanSolver(N);
+    }
+    else
+    {
+        for(char& c : input)
+        {
+           c = tolower(c);
+        }
+        vector<string> point_names = read_CSV(input + "_name.csv");
+        vector<pair<double, double>> points = read_CSV_points(input + "_xy.csv");
+        if (point_names.size() < 2 || points.size() < 2)
+        {
+            cout << "Insufficient files." << endl;
+            return -1;
+        }
+        solver = TravelingSalesmanSolver(point_names, points);
+    }
+
 
 #if TEST_PRINT
     solver.print_init_info();
@@ -675,9 +778,9 @@ int main()
     }
     cout << endl;
 
-    selected_paths = solver.roulette_selection(all_paths, 2);
+    selected_paths = solver.tournament_selection(all_paths, 2);
 
-    cout << endl << "Selected paths from roulette: ";
+    cout << endl << "Selected paths from tournament: ";
     cout << endl << "First (not by score): ";
     for (uint64_t i = 0; i < selected_paths[0].size(); i++)
     {
@@ -737,4 +840,5 @@ int main()
 
     Path result = solver.find_solution();
 
+    cout << MUTATION_CHANCE << endl;
 }
