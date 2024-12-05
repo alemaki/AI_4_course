866e4c90c3a1180fe176545467993e778f449402
Alexander Makedonski <alemak2002@gmail.com>
2024-12-05 17:16:10 +0200

    Updated classifier.

--- homeworks/AI_HW_5/src.py
+++ homeworks/AI_HW_5/src.py
@@ -12,12 +12,15 @@ def read_data_set(file_path: str) -> list[list[str]]:
         reader = csv.reader(data)
         return [row for row in reader]
 
-
-def preprocess_data(data: list[list[str]]) -> list[tuple[str, list[Optional[bool]]]]:
+def preprocess_attributes(attributes: list[str]) -> list[Optional[bool]]:
     def map_vote(vote: str) -> Optional[bool]:
         return {"y": True, "n": False}.get(vote, None)
     
-    dataset: list[tuple[str, list[Optional[bool]]]] = [(row[0], [map_vote(vote) for vote in row[1:]]) for row in data]
+    return [map_vote(vote) for vote in attributes]
+
+def preprocess_data(data: list[list[str]]) -> list[tuple[str, list[Optional[bool]]]]:
+    
+    dataset: list[tuple[str, list[Optional[bool]]]] = [(row[0], preprocess_attributes(row[1:])) for row in data]
 
     return dataset
 
@@ -26,26 +29,26 @@ VotesStats = dict[str, int]
 class BayesNC:
     def __init__(self):
         self.dataset_rows: int
-        self.main_classes: list[str]
+        self.class_names: list[str]
         self.attribute_counts: dict[str, dict[int, VotesStats]]
         self.class_counter: dict[int]
         self.dataset: list[tuple[str, list[Optional[bool]]]]
 
     def print_information_class_counter(self):
-        #df: DataFrame = DataFrame(self.class_counter_matrix, index = self.main_classes)
+        #df: DataFrame = DataFrame(self.class_counter_matrix, index = self.class_names)
         print(self.class_counter)
 
     def train_model(self, dataset: list[tuple[str, list[Optional[bool]]]]):
         self.dataset = deepcopy(dataset)
         self.dataset_rows = len(dataset)
-        self.main_classes = list(set(row[0] for row in dataset))
+        self.class_names = list(set(row[0] for row in dataset))
         attribute_count: int = len(dataset[0][1])
 
         self.attribute_counts = {
             class_name: [{"yes": 0, "no": 0, "none": 0} for _ in range(attribute_count)]
-            for class_name in self.main_classes
+            for class_name in self.class_names
         }
-        self.class_counter = {class_name: 0 for class_name in self.main_classes}
+        self.class_counter = {class_name: 0 for class_name in self.class_names}
 
         for class_name, attributes in dataset:
             self.class_counter[class_name] += 1
@@ -58,10 +61,37 @@ class BayesNC:
                 else:
                     self.attribute_counts[class_name][i]["no"] += 1
 
+    def get_attribute_total_for_class(self, class_name: str, attribute_index: int) -> int:
+        return self.attribute_counts[class_name][attribute_index]["none"] +\
+                self.attribute_counts[class_name][attribute_index]["yes"] +\
+                self.attribute_counts[class_name][attribute_index]["no"]
 
+    def get_votes_for_attribute_for_class(self, class_name: str, attribute_index: int, attribute: Optional[bool]) -> int:
+        if attribute == None:
+            return self.attribute_counts[class_name][attribute_index]["none"]
+        elif attribute == True:
+            return self.attribute_counts[class_name][attribute_index]["yes"]
         
-    
+        return self.attribute_counts[class_name][attribute_index]["no"]
+        
+    def get_model_prediciton(self, attributes: list[Optional[bool]]) -> str:
 
+
+        class_evals: dict[str, int] = {}
+        best_class_name: str = ""
+        for class_name in self.class_names:
+            chance: float = 1
+            for i, attribute in enumerate(attributes):
+                #total_votes_for_atr_in_class: int = self.get_attribute_total_for_class(class_name, i) #not interested now
+                votes_for_atr_in_class = self.get_votes_for_attribute_for_class(class_name, i, attribute)
+                if votes_for_atr_in_class == 0:
+                    votes_for_atr_in_class = 1.0 # Laplace smoothing 
+                votes_for_atr_in_class = float(votes_for_atr_in_class) #force float just in case
+                total_classes: float = float(self.class_counter[class_name])
+                chance *= votes_for_atr_in_class/total_classes
+                
+
+                
 
 
 
